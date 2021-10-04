14b5835a48c6c4f5fd3e3837d1cace1449ce6727
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-10-04 09:54:23 +0200

    Add files via upload

---
+++ Esercizi Classroom/9.cpp
@@ -0,0 +1,29 @@
+/*
+Website: https://matt05.ml
+GitHub: @Matt0550
+40. Dato un numero intero N, stabilire se è pari o dispari.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+string calcolo_pari_dispari(int numero) { // Funzione per determinare se il numero è pari o dispari
+    if (numero%2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
+
+int main()
+{
+    int N;
+
+    cout << "Inserire un numero intero ";
+    cin >> N;
+
+    cout << "Il numero è " << calcolo_pari_dispari(N);
+    return 0;
+}
+
