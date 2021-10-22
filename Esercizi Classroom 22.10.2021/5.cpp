bd26c9f21f952e4cee98a840b1f6a71535465e95
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-10-22 11:34:59 +0200

    Create 5.cpp

---
+++ Esercizi Classroom 22.10.2021/5.cpp
@@ -0,0 +1,24 @@
+/*
+Website: https://matt05.ml
+GitHub: @Matt0550
+5. Calcola il fattoriale di un numero
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout<<"Inserisci un numero intero: ";
+    cin>>N;
+    
+    int tot = N;
+    for (int i = 1; i < N && i > 0; i++) {
+        tot = tot * (N-i);
+    }
+    
+    cout<<tot;
+    return 0;
+}
