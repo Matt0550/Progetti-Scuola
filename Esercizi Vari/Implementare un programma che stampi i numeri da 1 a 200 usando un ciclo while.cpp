f611265f497959273b117122f0b8837dd468edc1
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-11-03 17:17:59 +0100

    Add files via upload

---
+++ Esercizi Vari/Implementare un programma che stampi i numeri da 1 a 200 usando un ciclo while.cpp
@@ -0,0 +1,19 @@
+/*
+Website: https://matt05.ml
+GitHub: @Matt0550
+Implementare un programma che stampi i numeri da 1 a 200 usando un ciclo while.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while(i<200) {
+        i++;
+        cout<<i<<endl;
+    }
+    return 0;
+}
