13bd3122e75642806ec252475effb2c280462151
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-03-17 10:39:04 +0100

    Update binary_search.cpp

--- Esercizi Vari/binary_search.cpp
+++ Esercizi Vari/binary_search.cpp
@@ -8,35 +8,39 @@ Ricerca binaria 17/03/2022
 
 using namespace std;
 
-int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-int cerca = 0;
-int a_len = sizeof(a)/sizeof(a[0]);
-int start = 0;
-int stop = a_len;
-bool trovato = false;
-int mediana = 0;
-int indice = -1;
-int main() {
-    cout<<"Che numero vuoi cercare? ";
-    cin>>cerca;
+int binary_search(int *array, int array_len, int cerca) {
+    int start = 0;
+    int stop = array_len;
+    int mediana = 0;
+    int indice = -1;
+    
     while(start <= stop) {
         mediana =  (start+stop)/2;
-        
-        if(a[mediana] == cerca) {
+        if(array[mediana] == cerca) {
             indice = mediana;
+            return indice;
             break;
-        } else if (a[mediana] < cerca) {
+        } else if (array[mediana] < cerca) {
             start = mediana + 1;
-            
         } else {
             stop = mediana - 1;
         }
     }
+    return indice;
+}
+
+int main() {
+    int cerca;
+    int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int a_len = sizeof(a)/sizeof(a[0]);
+    cout<<"Che numero vuoi cercare? ";
+    cin>>cerca;
+    int result = binary_search(a, a_len, cerca);
     
-    if(indice == -1) {
+    if(result == -1) {
         cout<<"\nNon trovato"<<endl;
     } else {
-        cout<<"Trovato alla posizione "<<indice;
+        cout<<"Trovato alla posizione "<<result;
     }
     return 0;
 }
