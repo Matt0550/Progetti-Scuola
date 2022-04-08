2a9429b270e248f515b2d8358512540fb7f57be7
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-04-08 10:33:07 +0200

    Update 6.cpp

--- Esercizi Classroom 30.03.2022/6.cpp
+++ Esercizi Classroom 30.03.2022/6.cpp
@@ -1,16 +1,21 @@
 /*
 Website: https://matt05.ml
 GitHub: @Matt0550
-
 6.Creare una funzione che riceve tre numeri interi e restituisce la differenza tra il più grande e il più piccolo.
 */
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int differenzaNumeri(int n1, int n2, int n3) {
-    return max(max(n2, n3), n1) - min(min(n2, n3), n1);
+    int numeri[3] = {n1, n2, n3};
+    int maggiore = numeri[0];
+    int minore = numeri[0];
+    for(int i = 1; i < 3; i++){
+        if(numeri[i] > maggiore) maggiore = numeri[i];
+        if(numeri[i] < minore) minore = numeri[i];
+    }
+    return maggiore-minore;
 }
 
 int main()
