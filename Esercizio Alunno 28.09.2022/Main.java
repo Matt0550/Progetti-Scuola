0f7803256433c3af046abfb591508124ab00ab11
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-09-28 10:39:18 +0200

    Create Main.java

---
+++ Esercizio Alunno 28.09.2022/Main.java
@@ -0,0 +1,22 @@
+/*
+Crere la alunno con i seguenti attributi: 
+- Nome
+- Cognome
+- Media dei voti 
+- Classe
+- Anni svolti
+
+altri attributi a piacere
+
+Metodi:
+- Stampa
+- Giorni di scuola mancanti
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		Alunno alunno1 = new Alunno("Gino", "Panino", "4C INF", "superiori", 4);
+	    alunno1.stampa();
+	}
+}
