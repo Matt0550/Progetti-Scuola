c99ac5e56575b3b6442b5d52c76230258899d454
Matteo <65899974+Matt0550@users.noreply.github.com>
2023-01-20 08:46:01 +0100

    Update Forma.java

--- Esercizio Classe Forma/Forma.java
+++ Esercizio Classe Forma/Forma.java
@@ -10,7 +10,5 @@ public class Forma {
 
     public void stampa() {
         System.out.println("Nome: " + nome);
-        System.out.println("Area: ????");
-        System.out.println("Perimetro: ????");
     }
 }
