6968dd7b64ff50207c7ff3b05921869c97b75e37
Matteo <65899974+Matt0550@users.noreply.github.com>
2023-01-25 10:38:53 +0100

    Update Triangolo.java

--- Esercizio Classe Forma/Triangolo.java
+++ Esercizio Classe Forma/Triangolo.java
@@ -5,10 +5,11 @@ public class Triangolo extends Forma {
     private double base;
     private double altezza;
 
-    public Triangolo(String nome, double base, double altezza, lato3) {
+    public Triangolo(String nome, double base, double altezza, double lato2, double lato3) {
         super(nome);
         this.base = base;
         this.altezza = altezza;
+        this.lato2 = lato2;
         this.lato3 = lato3;
     }
 
@@ -24,6 +25,6 @@ public class Triangolo extends Forma {
     }
 
     public double perimetro() {
-        return base + altezza + lato3;
+        return base + lato2 + lato3;
     }
 }
