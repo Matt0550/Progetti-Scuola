242a86f1453339aba55f80c97fbd76abe132d282
Matteo Sillitti <65899974+Matt0550@users.noreply.github.com>
2024-04-11 11:56:19 +0200

    Update index.php

--- esercizi_tpsit24/sestaEsercitazione/index.php
+++ esercizi_tpsit24/sestaEsercitazione/index.php
@@ -114,7 +114,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quinta esercitazione</title>
+    <title>Sesta esercitazione</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 
